337b7cdfaedf9e4cf46f5a7839da456fe143c3c0
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-01-27 19:58:10 +0100

    adding page limit

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,7 +1,7 @@
-from pyexpat.errors import messages
-
-from flask import Flask, jsonify, url_for, redirect, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -9,6 +9,24 @@ CORS(app)  # This will enable CORS for all routes
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
+    {"id": 3, "title": "Third post", "content": "This is the third post."},
+    {"id": 4, "title": "Fourth post", "content": "This is the fourth post."},
+    {"id": 5, "title": "Fifth post", "content": "This is the fifth post."},
+    {"id": 6, "title": "Sixth post", "content": "This is the sixth post."},
+    {"id": 7, "title": "Seventh post", "content": "This is the seventh post."},
+    {"id": 8, "title": "Eighth post", "content": "This is the eighth post."},
+    {"id": 9, "title": "Ninth post", "content": "This is the ninth post."},
+    {"id": 10, "title": "Tenth post", "content": "This is the tenth post."},
+    {"id": 11, "title": "Eleventh post", "content": "This is the eleventh post."},
+    {"id": 12, "title": "Twelfth post", "content": "This is the twelfth post."},
+    {"id": 13, "title": "Thirteenth post", "content": "This is the thirteenth post."},
+    {"id": 14, "title": "Fourteenth post", "content": "This is the fourteenth post."},
+    {"id": 15, "title": "Fifteenth post", "content": "This is the fifteenth post."},
+    {"id": 16, "title": "Sixteenth post", "content": "This is the sixteenth post."},
+    {"id": 17, "title": "Seventeenth post", "content": "This is the seventeenth post."},
+    {"id": 18, "title": "Eighteenth post", "content": "This is the eighteenth post."},
+    {"id": 19, "title": "Nineteenth post", "content": "This is the nineteenth post."},
+    {"id": 20, "title": "Twentieth post", "content": "This is the twentieth post."}
 ]
 
 
@@ -23,12 +41,31 @@ def get_posts():
     direction = request.args.get("direction", "").lower()
     sort_values = ["title", "content"]
     direction_values = ["asc", "desc"]
-    if not sort and not direction:
+    page = request.args.get("page")
+    limit = request.args.get("limit")
+    if not sort and not direction and not page and not limit:
         return jsonify(POSTS)
     elif sort in sort_values and direction in direction_values:
         direction = direction != "asc"
         sorted_posts = sorted(POSTS, key=lambda post:post[sort].lower(), reverse=direction)
         return jsonify(sorted_posts)
+    elif page and limit:
+        try:
+            page = int(request.args.get('page', 1))
+            limit = int(request.args.get('limit', 10))
+        except ValueError:
+            return jsonify({
+                "error": "Invalid page or limit parameter"
+            }), 400
+        if page < 1 or limit < 1:
+            return jsonify({"error": "Page and limit must be greater than 0"}), 400
+
+        start_index = (page - 1) * limit
+        end_index = start_index + limit
+
+        paginated_posts = POSTS[start_index:end_index]
+
+        return jsonify(paginated_posts)
     else:
         return jsonify({
             "error": "Bad Request",
